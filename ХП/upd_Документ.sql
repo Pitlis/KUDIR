38fc4e56a044310adeecb5d614b5a68d0d27a69b
pitlis <pitlis95@gmail.com>
2015-04-08 23:12:17 +0300

    Переименовал файлы с представлениями. Процедуры и представления теперь в txt файлах.

--- ХП/upd_Документ.sql
+++
@@ -1,7 +0,0 @@
-CREATE PROCEDURE upd_Документ
-@Номер VARCHAR(30),
-@Наименование VARCHAR(100),
-@Дата DATETIME,
-@ID INT
-AS
-UPDATE Документ SET Номер = @Номер, Наименование = @Наименование, Дата = @Дата WHERE DocumentID = @ID
